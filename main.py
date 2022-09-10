9821433bb007f6bc29f7998e931313f5c7c539fb
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-10 17:59:54 +0200

    Use grids

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ x = 0
 win = tk.Tk()
 
 number = tk.Label(win, text = x)
-number.pack()
+number.grid(row= 1, column=1, columnspan=2)
 
 increment = tk.Button(win, text = "INCREMENT", width=20)
-increment.pack()
+increment.grid(row = 2, column = 1)
 
 decrement = tk.Button(win, text = "DECREMENT", width=20)
-decrement.pack()
+decrement.grid(row=2, column=2)
+
 win.mainloop()
