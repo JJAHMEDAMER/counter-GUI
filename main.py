a24025f90c4db661ec3d1ed1a05e4a5f34841884
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-10 17:48:46 +0200

    Add Buttons

--- main.py
+++ main.py
@@ -6,4 +6,9 @@ win = tk.Tk()
 number = tk.Label(win, text = x)
 number.pack()
 
+increment = tk.Button(win, text = "INCREMENT", width=20)
+increment.pack()
+
+decrement = tk.Button(win, text = "DECREMENT", width=20)
+decrement.pack()
 win.mainloop()
