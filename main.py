2fa6ac6dbba1f67bdac9accd8de698cc2ebc6f1b
JJAHMEDAMER <ahmedamer703.aa@gmail.com>
2022-09-10 20:00:52 +0200

    use config method

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from ast import Global
 import tkinter as tk
 
 def plus_one():
     global x
     x += 1
-    number = tk.Label(win, text = x)
+    number.config(text = x)
     number.grid(row= 1, column=1, columnspan=2)
-
+    
 def minus_one():
     global x
     x -= 1
-    number = tk.Label(win, text = x)
+    number.config(text = x)
     number.grid(row= 1, column=1, columnspan=2)
-
 
 x = 0
 win = tk.Tk()
